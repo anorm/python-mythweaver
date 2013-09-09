cb2f196342c550de7ef4de5833605860b60a6b84
Anders Norman <anders.norman@barco.com>
2013-09-09 15:51:54 +0200

    PEP8 cleanup

--- mythweaver.py
+++ mythweaver.py
@@ -4,12 +4,13 @@ import requests
 import sys
 from lxml import etree
 
+
 class MythWeaver:
     def login(self, username, password):
         self.session = requests.Session()
-        phash=md5.new(password).hexdigest()
-        url="http://www.myth-weavers.com/login.php?do=login"
-        opts={
+        phash = md5.new(password).hexdigest()
+        url = "http://www.myth-weavers.com/login.php?do=login"
+        opts = {
             'do':                       'login',
             'vb_login_username':        username,
             'vb_login_md5password':     phash,
@@ -18,14 +19,13 @@ class MythWeaver:
             'security_token':           'guest'
         }
         response = self.session.post(url, data=opts)
-        return response.cookies.get_dict().has_key('bbsessionhash')
+        return 'bbsessionhash' in response.cookies.get_dict()
 
     def downloadSheet(self, sheetid):
-        url="http://www.myth-weavers.com/sheet.php"
-        opts={
+        url = "http://www.myth-weavers.com/sheet.php"
+        opts = {
             'do':      'download',
             'sheetid': sheetid
         }
         response = self.session.post(url, data=opts)
         return etree.fromstring(response.content)
-
